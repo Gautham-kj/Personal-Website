c0b72476a2200be7f04c85a9b0d4a073bb40257a
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-12-10 13:41:45 +0530

    added Light mode and dark mode to Landing page

--- frontend/src/Components/Navbar.svelte
+++ frontend/src/Components/Navbar.svelte
@@ -9,41 +9,40 @@
         <div class="flex items-center p-4 dark:hover:fill-white dark:fill-green-500  hover:fill-black fill-green-300">
             <a href="/">
                 <button class="flex pl-3 ">
-                    <a href="/" >
-                        <button>
-                            <h1 class="flex md:text-4xl text-2xl items-center font-bold text-green-500 font-avarabold">
-                                Gautham_KJ
-                            </h1>
-                        </button>
-                    </a>
+                    <button>
+                        <h1 class="flex md:text-4xl text-2xl items-center font-bold dark:text-green-500 text-orange-500 font-avarabold">
+                            Gautham_KJ
+                        </h1>
+                    </button>
+                
                 </button>
             </a>
         </div>
         <!-- Navigation Buttons -->
         <div class="flex pr-6 space-x-6 text-xl font-avarabold text-white  items-center ">
             <div class="hidden md:flex">
-                <a href="#ProjectDetails">
-                    <button class="dark:hover:text-white hover:text-black rounded-xl border-white bg-slate-400 hover:bg-opacity-20 bg-opacity-0 p-3" >
+                <a href="/aboutme">
+                    <button class="text-black dark:text-white hover:text-black rounded-xl border-white bg-slate-400 hover:bg-opacity-20 bg-opacity-0 p-3" >
                         About Me
                     </button >
                 </a>
                 <a href="/">
-                    <button class="dark:hover:text-white hover:text-black rounded-xl border-white bg-slate-400 hover:bg-opacity-20 bg-opacity-0 p-3" >
+                    <button class="text-black dark:text-white hover:text-black rounded-xl border-white bg-slate-400 hover:bg-opacity-20 bg-opacity-0 p-3" >
                         Projects
                     </button>
                 </a>
             </div>
             <!-- Darkmode Button -->
-            {#if darkmode==true}
+            {#if darkmode!=true}
                 <button on:click={()=>(darkmode= !darkmode)} class="rounded-xl border-white bg-slate-400 hover:bg-opacity-20 bg-opacity-0 p-3">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="orange" class="w-6 h-6 hover:fill-yellow-200 ">
-                    <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-                </svg>  
-            </button>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="orange" class="w-6 h-6 hover:fill-red-400 ">
+                        <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                    </svg>  
+                </button>
             {:else}
                     <button on:click={()=>(darkmode= !darkmode)} class="rounded-xl border-white bg-slate-300 hover:bg-opacity-20 bg-opacity-0 p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="purple" class="w-6 h-6 hover:fill-black">
-                        <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clip-rule="evenodd" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="purple" class="w-6 h-6 hover:fill-white">
+                            <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clip-rule="evenodd" />
                         </svg>                
                     </button>
             {/if}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,40 +1,42 @@
 <script lang="ts">
 	import Navbar from "../Components/Navbar.svelte";
-    // import ProjectDetails from "../Components/ProjectDetails.svelte";
+    import ProjectDetails from "../Components/ProjectDetails.svelte";
+    import Bottom from "../Components/Bottom.svelte";
     let darkmode:boolean;
     let frontmount="src/lib/images/frontmount.svg";
     let backmount="src/lib/images/backmount.svg";
     let sky="src/lib/images/sky.svg";
     let Realmoon="src/lib/images/RealMoon.svg";
-    let scroll:number;
-    let width:number;
+    let RealSun= "src/lib/images/RealSun.svg";
+    let scroll:number=0;
+    let width:number=0;
+    let sunscroll:number=(width*0.75-scroll*1.3);
+    let height:number;
     </script>
-<svelte:window bind:scrollY={scroll} bind:innerWidth={width}/>
-<div class:darkmode class="{darkmode?"dark":""} bg-mainbg" >
-    <Navbar bind:darkmode />
-    <div class="w-screen " style:height={`${0.5625*width}`}>
-        <div class=" w-full overflow-x-hidden bg-nightsky">
-                <img src={sky} alt="sky" class="w-full overflow-hidden z-8 absolute bg-nightsky" style:transform={`translate3d(0,-${scroll*0.3}px,0)`}/>
-                <img src={backmount} alt="backmountain" class="w-full transform overflow-hidden z-1 absolute" />
-                <div class="w-full py-16 md:py-24 absolute overflow-hidden">
-                    <img src={Realmoon} alt="Moon" class="w-20 md:w-40 -z-5 transform overflow-hidden " style:transform={`translate3d(${scroll}px,0,0)`}/>
-                </div>
-                <img src={frontmount} alt="frontmountain" class="w-full overflow-x-hidden absolute z-3" />
-                
+<svelte:window bind:scrollY={scroll} bind:innerWidth={width} bind:innerHeight={height}/>
+<body class="bg-mainbg">
+    <div class:darkmode class="{darkmode?"dark":""} bg-mainbg " >
+        <Navbar bind:darkmode />
+        <div class="w-screen " style:height={`${0.5625*width}`}>
+            <div class=" w-full overflow-x-hidden bg-blue-400">
+                    <img src={sky} alt="sky" class="w-full dark:visible invisible overflow-hidden z-8 absolute bg-nightsky" style:transform={`translate3d(0,-${scroll*0.3}px,0)`}/>
+                    <img src={backmount} alt="backmountain" class="w-full  transform overflow-hidden dark:bg-transparent bg-blue-400 z-1 absolute" />
+                    <div class="flex w-full pt-10 md:py-24 absolute overflow-hidden">
+                        {#if darkmode}
+                            <img src={Realmoon} alt="Moon" class="flex py-4 w-20 md:w-40 overflow-hidden " style:transform={`translate3d(${scroll*1.3}px,0,0)`}/>
+                        {:else}
+                            <img src={RealSun} alt="Sun" class=" w-40 md:w-80 items-end overflow-hidden" style:transform={`translate3d(${sunscroll*0.5}px,0,0)`}/>
+                        {/if}
+                    </div>
+                    <img src={frontmount} alt="frontmountain" class="w-full  overflow-x-hidden absolute z-3" />  
+            </div>
         </div>
-        
     </div>
-</div>
-<main class=" p-4 bg-mainbg " id="about" style:transform={`translate3d(0,${0.5625*width}px,0)`}>
-            <h1 class="font-avarabold text-3xl text-white text-semibold" >
-                About
-            </h1>
-            <br>
-            <br>
-            <br>
-            <p class="font-avarablack text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            <br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            <br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-</main>
+    <div class="pt-8 p-4 bg-mainbg " id="about" style:transform={`translate3d(0,${0.5625*width-5}px,0)`}>
+        <ProjectDetails/>
+    </div>
+    <h1>{height}</h1>
+</body>
+<!-- <div class="h-80 w-80 bg-white" style:transform={`translate3d(0,${height*1.05}px,0)`}>
+    <Bottom/>
+</div> -->
